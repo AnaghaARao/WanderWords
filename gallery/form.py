0d17e7426f3b4f44d2658d3ae5299526a69c16f1
AnaghaARao <rao.anagha2002@gmail.com>
2024-05-05 21:04:11 +0530

    Created form to edit blog

---
+++ gallery/form.py
@@ -0,0 +1,9 @@
+# Creating a form to edit a blog when user requests
+
+from django import forms
+from .models import Product
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fiels = ['name','description','image']
