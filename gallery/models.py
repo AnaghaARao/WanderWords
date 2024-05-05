8b4e767070f1f77fa9befb6367482bf1ee5909ba
AnaghaARao <rao.anagha2002@gmail.com>
2024-05-05 20:50:03 +0530

    Created a Product table with diff. fields

--- gallery/models.py
+++ gallery/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+# Here we have created a Product table with name, 
+# description, image, created_at, and updated_at fields in the table.
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255)
